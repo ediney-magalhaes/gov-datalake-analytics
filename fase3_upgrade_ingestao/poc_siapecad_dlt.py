97b2519d9ff310e7945ce6dcd564674b051b630b
Ediney Magalhães <edy.estatistica@gmail.com>
2026-03-07 15:39:27 -0400

    feat(api): atualiza layout do dlt para bronze_raw e injeta metadados de governanca

--- fase3_upgrade_ingestao/poc_siapecad_dlt.py
+++ fase3_upgrade_ingestao/poc_siapecad_dlt.py
@@ -3,6 +3,13 @@ import requests
 import logging
 import sys
 import os
+import hashlib
+
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+salt = os.getenv('HASH_SALT')
 
 #preparação do formatador do arquivo de log
 formatador = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -26,7 +33,7 @@ logging.getLogger().addHandler(tela_log)
 
 #destino
 bucket_url = 'file:' + os.path.abspath('data_lake_local')
-destino = dlt.destinations.filesystem(bucket_url, layout="{table_name}/year=2026/month=03/{load_id}")
+destino = dlt.destinations.filesystem(bucket_url, layout="bronze_raw/{table_name}/year=2026/month=03/{load_id}")
 
 #construindo pipeline de destino do dado
 pipeline_siapecad = dlt.pipeline(pipeline_name='siapecad' , destination= destino , dataset_name='bronze_siapecad' )
@@ -41,7 +48,18 @@ def conectar_api_siapecad():
     try:
         resposta = requests.get(url_api, headers= cabecalhos, verify=False, timeout=10)
         resposta.raise_for_status()
-        yield resposta.json()
+        dados_api = resposta.json()
+        #injetar metadados linha a linha in-flight
+        for linha in dados_api:
+            linha['source_system'] = 'siape_cad'
+            linha['ingestion_timestamp'] = datetime.now().isoformat()
+
+            #caso haja coluna CPF faz hash com salt
+            if 'cpf' in linha:
+                linha['hash_cpf'] = hashlib.sha256((str(linha['cpf']) + str(salt)).encode('utf-8')).hexdigest()
+                #deleta cpf original por segurança
+                del linha['cpf']
+        yield dados_api
     except requests.exceptions.RequestException as erro:
         logging.error(f'O tempo de conexão falhou: {erro}')
         yield []
