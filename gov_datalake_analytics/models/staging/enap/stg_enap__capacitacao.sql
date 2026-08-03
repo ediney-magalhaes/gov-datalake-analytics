13ecaec680f30c78450ed6b6e02972a9d2368105
ediney-magalhaes <edy.estatistica@gmail.com>
2026-08-03 11:36:42 -0400

    feat: cria modelo de staging stg_enap__capacitacao com tratamento de datas (sem linkage com siape - codigo_pessoa e id proprietario da enap)

---
+++ gov_datalake_analytics/models/staging/enap/stg_enap__capacitacao.sql
@@ -0,0 +1,16 @@
+with source as(
+    select *
+    from {{ source('enap', 'enap_capacitacao') }}
+),
+
+final as(
+    select
+        {{ dbt_utils.star(from=source('enap', 'enap_capacitacao'), except=['dt_matricula', 'dt_inicio', 'dt_fim', 'dt_inicio_insc', 'dt_fim_insc']) }},
+        safe.parse_datetime('%Y-%m-%d %H:%M:%S', dt_matricula) as dt_matricula,
+        safe.parse_datetime('%Y-%m-%d %H:%M:%S', dt_inicio) as dt_inicio,
+        safe.parse_datetime('%Y-%m-%d %H:%M:%S', dt_fim) as dt_fim,
+        safe.parse_date('%Y-%m-%d', dt_inicio_insc) as dt_inicio_inscricao,
+        safe.parse_date('%Y-%m-%d', dt_fim_insc) as dt_fim_inscricao
+    from source
+)
+select * from final
